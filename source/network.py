bc781f1fa3834b32263b669f98e016ab4f72b007
Cole Howard <cole@subtlegears.com>
2015-10-01 06:53:31 -0700

    First successful backprop

--- source/network.py
+++ source/network.py
@@ -11,11 +11,11 @@ from random import choice
 from sklearn import datasets
 
 
-
 class Network:
 
-    def __init__(self, neuron_targets, vector_size, train_set, train_answers, 
-                test_set, test_answers, epoch_nums=100, layers=1, learn_iter=1):
+    def __init__(self, neuron_targets, vector_size, train_set, train_answers,
+                 test_set, test_answers, epoch_nums=100, layers=1,
+                 learn_iter=1):
         """ A Network instance will create layers of neurons for the implementa-
         tion of neural network.
 
@@ -40,8 +40,8 @@ class Network:
         self.epoch_nums = epoch_nums
         self.layers = layers
         self.learn_iter = learn_iter        # Size of leaning sets/iteration
-        self.neurons = [Neuron(self.vector_size, x, len(self.train_set), self.train_answers) 
-                        for x in self.neuron_count]
+        self.neurons = [Neuron(self.vector_size, x, len(self.train_set),
+                        self.train_answers) for x in self.neuron_count]
         self.predictions = []
 
     def mse(self, answer_vector, result_vector):
@@ -58,16 +58,16 @@ class Network:
                     range(len(answer_vector))]) / len(answer_vector)
 
     def gradient_descent(self, vector, vector_index):
-        """ Calculates the gradient_descent 
-        
+        """ Calculates the gradient_descent
+
         Args:
             vector: a list
-            vector_index: an int  
+            vector_index: an int
 
         Returns:
             an int
         """
-        learning_rate = .1
+        learning_rate = .05
         temp_list = [(self.neurons[x]._sigmoid(self.neurons[x]._dot_product(vector)) - self.neurons[x].expected[vector_index]) * self.neurons[x]._sigmoid(self.neurons[x]._dot_product(vector)) * (1 - self.neurons[x]._sigmoid(self.neurons[x]._dot_product(vector))) for x in self.neuron_count]
         gd = -1 * learning_rate * sum(temp_list)
         return gd
@@ -75,36 +75,41 @@ class Network:
     def learn_run(self):
         """ Runs an iteration through the neuron sets and adjust the weights
         appropriately.
-        """ 
+        """
         for idx, vector in enumerate(self.train_set):
             for neuron in self.neurons:
                 neuron.train_pass(vector, idx)
-            gd = self.gradient_descent(vector, idx)       # Backpropogate the error
+            gd = self.gradient_descent(vector, idx)    # Backpropogate the error
             for neuron in self.neurons:
                 neuron.update_weights(gd, vector)
 
     def run_unseen(self):
-        """ Makes guesses on the unseen data """
+        """ Makes guesses on the unseen data
+
+        Returns:
+            a list of ints (the guesses for each vector)
+
+        """
 
         temp_guess_list = [[] for x in self.test_set]
-        temp_nofire_guess_list = [[] for x in self.test_set]
+        temp_dud_guess_list = [[] for x in self.test_set]
         for idy, vector in enumerate(self.test_set):
-            
+
             for idx, neuron in enumerate(self.neurons):
                 nf = neuron.fires(vector)
                 if nf[0]:
                     temp_guess_list[idy].append((nf[1], idx))
+                    temp_dud_guess_list[idy].append((0, idx))
                 else:
                     temp_guess_list[idy].append((0, None))
-                    temp_nofire_guess_list[idy].append((nf[1], idx))
+                    temp_dud_guess_list[idy].append((nf[1], idx))
             temp_guess_list[idy].sort(reverse=True)
-            temp_nofire_guess_list[idy].sort(reverse=True)
-        guess_list = [temp_guess_list[x][0][1] for x in range(len(self.test_set))]
-        nofire_guess_list = [temp_nofire_guess_list[x][0][1] for x in range(len(self.test_set))]
-        if temp_guess_list[0][0]:
-            return guess_list
-        else:
-            return nofire_guess_list
+            temp_dud_guess_list[idy].sort(reverse=True)
+        guess_list = [x[0][1] for x in temp_guess_list]
+        dud_guess_list = [x[0][1] for x in temp_dud_guess_list]
+        new_guess_list = [x if (x != None) else dud_guess_list[idx] for
+                          idx, x in enumerate(guess_list)]
+        return new_guess_list
 
     def report_results(self, guess_list):
         """ Reports results of guesses on unseen set
@@ -129,31 +134,35 @@ class Neuron:
             vector_size: int
             target: int
             sample_size: int
-            answer_set: list 
+            answer_set: list
         """
         self.threshold = .5
         self.answer_set = answer_set
-        self.target = target 
+        self.target = target
         self.weights = [0 for x in range(vector_size + 1)]
         self.weights[-1] = 1
         self.sample_size = sample_size
         self.expected = [0 if y != self.target else 1 for y in self.answer_set]
         self.guesses = [0 for z in range(self.sample_size)]
 
-    
     def train_pass(self, vector, idx):
-        """ Passes a vector through the neuron once 
+        """ Passes a vector through the neuron once
 
         Args:
             vector: a list
-            idx: an int         # The position of the vector in the sample 
-        """ 
-        if self.fires(vector)[0]:
-            self.guesses[idx] = 1
+            idx: an int         # The position of the vector in the sample
+        """
+        if self.expected == self.guesses:
+            return None
         else:
-            self.guesses[idx] = 0
-        error = self.expected[idx] - self.guesses[idx]
-        self.update_weights(error, vector)
+            error = self.expected[idx] - self.guesses[idx]
+            if self.fires(vector)[0]:
+                self.guesses[idx] = 1
+            else:
+                self.guesses[idx] = 0
+            self.update_weights(error, vector)
+            return None
+#### Look at this again
 
     def _dot_product(self, vector):
         """ Returns the dot product of two equal length vectors
@@ -167,10 +176,12 @@ class Neuron:
         return sum(elem * weight for elem, weight in zip(vector, self.weights))
 
     def _sigmoid(self, z):
-        try:
+        if -700 < z < 700:
             return 1 / (1 + e ** (-z))
-        except:
+        elif z < -700:
             return 0
+        else:
+            return 1
 
     def update_weights(self, error, vector):
         """ Updates the weights stored in the receptors
@@ -192,14 +203,14 @@ class Neuron:
         pass
 
     def fires(self, vector):
-        """ Takes an input vector and decides if neuron fires or not 
-        
+        """ Takes an input vector and decides if neuron fires or not
+
         Args:
-            vector - a list 
+            vector - a list
 
         Returns:
             a boolean
-            a float             # The dot product of the vector and weights 
+            a float             # The dot product of the vector and weights
         """
         dp = self._dot_product(vector)
         if dp > self.threshold:
@@ -207,7 +218,7 @@ class Neuron:
             return True, dp
         else:
             return False, dp
-        
+
 
 def append_bias(vector):
     """ Takes a list of n entries and appends a 1 for the bias
@@ -222,19 +233,22 @@ def append_bias(vector):
     temp_vector.append(1)
     return temp_vector
 
+
 def main():
     # Dependent on input set
     digits = datasets.load_digits()
     target_values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    answers, answers_to_test = digits.target[:1000], digits.target[1000:]
-    training_set, testing_set = digits.data[:1000], digits.data[1000:]
+    num_of_training_vectors = 950
+    answers, answers_to_test = digits.target[:num_of_training_vectors], digits.target[num_of_training_vectors:]
+    training_set, testing_set = digits.data[:num_of_training_vectors], digits.data[num_of_training_vectors:]
 
     # For all inputs
     training_vectors = [append_bias(vector) for vector in training_set]
     test_vectors = [append_bias(vector) for vector in testing_set]
 
-    network = Network(target_values, len(training_set[0]), training_vectors, answers, test_vectors, answers_to_test)
-    [network.learn_run() for x in range(500)]
+    network = Network(target_values, len(training_set[0]), training_vectors,
+                      answers, test_vectors, answers_to_test)
+    [network.learn_run() for x in range(250)]
     network.report_results(network.run_unseen())
 
 if __name__ == '__main__':
