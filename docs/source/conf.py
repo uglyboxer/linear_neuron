2aed041ed5449db48dfa9ebefb4e0a30042101e9
Cole Howard <cole@subtlegears.com>
2015-10-08 12:47:52 -0700

    Add mocks for import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,12 @@
 import sys
 import os
 import shlex
+import mock
+ 
+MOCK_MODULES = ['numpy', 'matplotlib.pyplot', 'sklearn.utils',  'sklearn.datasets']
+]
+for mod_name in MOCK_MODULES:
+sys.modules[mod_name] = mock.Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
